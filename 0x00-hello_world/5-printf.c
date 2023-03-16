13b2e95d292c808588eb6a80bf294f950b81884e
Hassan Cissey <hcissey0@gmail.com>
2023-03-16 11:59:13 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: Prints text with printf.
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

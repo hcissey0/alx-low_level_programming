a4f9de7d4ef7b3c545881685a170a9e6e08a8b04
Hassan Cissey <hcissey0@gmail.com>
2023-03-17 06:39:17 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers from 0 to 9 with putcar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}

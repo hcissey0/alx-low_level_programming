eaf3eb06fb2ccf2a743c5455ec22d2b404320176
Hassan Cissey <hcissey0@gmail.com>
2023-03-17 07:00:39 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+		}
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
+}
+
+

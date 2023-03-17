7f27c0d706cc409b607eb257e186c88373ff7eb0
Hassan Cissey <hcissey0@gmail.com>
2023-03-17 07:54:56 -0700

    Print combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints uniwue combinations
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b = 48, c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		b++;
+		for (c = b; c <= 57; c++)
+		{
+			putchar(a);
+			putchar(c);
+			if (a != '8' && c != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}

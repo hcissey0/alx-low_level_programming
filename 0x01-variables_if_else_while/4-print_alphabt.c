f15c09473b6b4b0dfad19822c86edd71505138f4
Hassan Cissey <hcissey0@gmail.com>
2023-03-17 06:26:47 -0700

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (a != 'q' || a != 'e')
+			putchar(a);
+	}
+
+	return (0);
+}

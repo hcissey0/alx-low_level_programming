5873aa1b27cfbd44a7ce1d3c428626b01ed3c4a7
Hassan Cissey <hcissey0@gmail.com>
2023-03-21 07:26:29 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
+#ifndef _UNISTD_H_
+#define _UNISTD_H_
+
 /**
  * _putchar - prints a character to the screen
+ * @c: character to be printed
  *
  * Return: Always 1 (Success)
  */
 int _putchar(char c);
+
+#endif /* _UNISTD_H_ */

2ceecc60952ade0d4da955e452a25e889114c8c7
Hassan Cissey <hcissey0@gmail.com>
2023-03-21 14:43:05 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 1, b = 2, c, i = 0;
+
+	do {
+		printf("%d", a);
+		a = b;
+		b = c;
+		c = a + b;
+		if (i != 50)
+			printf(", ");
+		i++;
+	} while (i <= 50);
+	return (0);
+}

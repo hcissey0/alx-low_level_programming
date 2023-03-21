30d80ccbe3d8b798f6260975064e55e22932ba71
Hassan Cissey <hcissey0@gmail.com>
2023-03-21 16:02:14 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,10 @@
  */
 void print_times_table(int n)
 {
-	if (n >= 0 && n < 16)
-		return;
 	int a, b, c;
 
+	if (n >= 0 && n < 16)
+		return;
 	for (a = 0; a <= n; a++)
 	{
 		for (b = 0; b <= n; b++)

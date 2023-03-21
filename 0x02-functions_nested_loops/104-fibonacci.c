b7a2911fcaf09ebe0b1e9c6177902eba4cb26ea7
Hassan Cissey <hcissey0@gmail.com>
2023-03-21 15:15:33 -0700

    More fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int a = 1, b = 2, c, i = 0;
+
+	c = a + b;
+	do {
+		printf("%u", a);
+		a = b;
+		b = c;
+		c = a + b;
+		if (i != 97)
+			printf(", ");
+		i++;
+	} while (i < 98);
+	printf("\n");
+	return (0);
+}

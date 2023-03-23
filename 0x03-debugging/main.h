a2aa5e735822befcc4471a6bd7e6ba12f65ff505
Hassan Cissey <hcissey0@gmail.com>
2023-03-23 07:29:06 +0000

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,12 +3,6 @@
 
 #include <stdio.h>
 
-/**
- * positive_or_negative - checks if a number is positive or negative
- * @i: number to be checked
- *
- * Return: void
- */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */

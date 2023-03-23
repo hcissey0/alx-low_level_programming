e93cbea57efbf760bf808ebf6d52e854e59ce5c6
Hassan Cissey <hcissey0@gmail.com>
2023-03-23 20:22:42 +0000

    Prime Factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,43 +1,30 @@
 #include <stdio.h>
 
 /**
- * is_prime - checks if a number is prime
- * @n: num to be checked
- * Return: 1 if true, 0 otherwise
- */
-int is_prime(int n)
-{
-	int i;
-
-	if (n <= 1)
-		return (0);
-	if (n <= 3)
-		return (1);
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
-	for (i = 0; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-	}
-
-	return (1);
-}
-
-/**
  * main - entry point
  * Return: 0 always
  */
 int main(void)
 {
 	int i, max;
+	unsigned long long n = 612852475143;
 
-	for (i = 1; i * i <= 612852475143; i += 2)
+	while (n % 2 == 0)
 	{
-		if (is_prime(i))
-			max = i;
+		max = 2;
+		n /= 2;
 	}
+
+	for (i = 3; i < n; i += 2)
+	{
+		if (n % i == 0)
+		{
+			max = i;
+			n /= i;
+		}
+	}
+
+	printf("%d\n", max);
 
 	return (0);
 }

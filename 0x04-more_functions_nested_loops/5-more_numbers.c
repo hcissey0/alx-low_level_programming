eddd707df2180ea78617f253c22fe88f21b38afa
Hassan Cissey <hcissey0@gmail.com>
2023-03-23 05:26:08 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints form 0 to 14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 15; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+				_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+		}
+		_putchar('\n');
+	}
+}

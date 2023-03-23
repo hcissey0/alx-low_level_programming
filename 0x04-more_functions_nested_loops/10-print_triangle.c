b923f2996c56623af04a6ddd7433805c5bfc4a3d
Hassan Cissey <hcissey0@gmail.com>
2023-03-23 08:56:06 -0700

    TRIANGLE

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,11 +3,13 @@
 /**
  * print_triangle - prints a triangle
  * @size: size of the triangle
+ *
  * Return: void
  */
 void print_triangle(int size)
 {
 	int i, j;
+
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)

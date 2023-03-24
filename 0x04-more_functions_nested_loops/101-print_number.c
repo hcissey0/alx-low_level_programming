266e9f08e9f90d4ff79d458bf09fe0c75c3e4bea
Hassan Cissey <hcissey0@gmail.com>
2023-03-24 03:48:29 +0000

    Print Numbera

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		i = -i;
 	}
 
-	if (n >= 10)
-		print_number(n / 10);
+	if (i >= 10)
+		print_number(i / 10);
 
-	_putchar(48 + n % 10);
+	_putchar(48 + i % 10);
 }

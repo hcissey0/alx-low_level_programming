14f5eea55c18d107a757beda27a26ccc6bcacb80
Hassan Cissey <hcissey0@gmail.com>
2023-03-27 21:01:12 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  */
 int _atoi(char *s)
 {
-	int i, neg = 0, res;
+	int i, neg = 0, res = 0;
 	unsigned ans = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -25,7 +25,9 @@ int _atoi(char *s)
 		}
 	}
 
+	res = ans;
 	if (neg % 2 != 0)
 		res = ans * -1;
-	return (ans);
+
+	return (res);
 }

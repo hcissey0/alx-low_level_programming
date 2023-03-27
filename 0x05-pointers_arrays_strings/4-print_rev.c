a380d480b76fcfbf58f35514d00a3450a8f5b0e2
Hassan Cissey <hcissey0@gmail.com>
2023-03-27 14:31:10 +0000

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	for (i = 0; s[i] != '\0'; i ++)
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}

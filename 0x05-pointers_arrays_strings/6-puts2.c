e4a02e6ad68203b8075afe5a9fbd35737745ec9d
Hassan Cissey <hcissey0@gmail.com>
2023-03-27 17:37:43 +0000

    Print Odd

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints odd indexes
+ * @str: to be printed
+ */
+void puts2(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	for (; j < i; j++)
+	{
+		if (j % 2 == 0)
+			continue;
+		_putchar(str[j]);
+	}
+}

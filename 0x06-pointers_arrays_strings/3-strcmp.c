15cc468db90a3874ce0064cbba5d49c1614f20bf
Hassan Cissey <hcissey0@gmail.com>
2023-03-29 13:49:46 -0700

    Strcomp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		sum1 += s1[i];
-
-	for (i = 0; s2[i] != '\0'; i++)
-		sum2 += s2[i];
-
-	return (sum1 - sum2);
+	while (s1[i] != '\0\ && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }

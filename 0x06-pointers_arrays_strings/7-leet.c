0fb874f24cc158c038829f574fa0c6ab24586605
Hassan Cissey <hcissey0@gmail.com>
2023-03-29 16:35:37 +0000

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - converts text to leet
+ * @s: string to change
+ * Return: character pointer
+ */
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		char c = s[i];
+		int a = (s[i] == 'a' || s[i] == 'A');
+		int e = (s[i] == 'e' || s[i] == 'E');
+		int o = (s[i] == 'o' || s[i] == 'O');
+		int t = (s[i] == 't' || s[i] == 'T');
+		int l = (s[i] == 'l' || s[i] == 'L');
+
+		s[i] = (a * '4') + (e * '3') + (o * '0') + (t * '7') + (l * '1') + (!(a || e || o || t || l) * c);
+	}
+
+	return (s);
+}

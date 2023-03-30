a56c90a6967505f3f8898aff839efa0fb62f20b4
Hassan Cissey <hcissey0@gmail.com>
2023-03-30 19:41:44 +0000

    Add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,41 +16,34 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	len1 = 0;
 	while (n1[len1])
 		len1++;
-
 	len2 = 0;
 	while (n2[len2])
 		len2++;
-
 	if (len1 + 1 > size_r || len2 + 1 > size_r)
 		return (0);
-
 	k = 0;
 	carry = 0;
 	i = len1 - 1;
 	j = len2 - 1;
-
 	while (i >= 0 || j >= 0 || carry > 0)
 	{
 		int d1 = i >= 0 ? n1[i] - '0' : 0;
 		int d2 = j >= 0 ? n2[j] - '0' : 0;
-
 		int sum = d1 + d2 + carry;
 
 		if (k >= size_r - 1)
 			return (0);
 		r[k] = (sum % 10) + '0';
 		carry = sum / 10;
-
 		i--;
 		j--;
 		k++;
 	}
-
 	r[k] = '\0';
-
 	for (i = 0, j = k - 1; i < j; i++, j--)
 	{
 		char tmp = r[i];
+
 		r[i] = r[j];
 		r[j] = tmp;
 	}

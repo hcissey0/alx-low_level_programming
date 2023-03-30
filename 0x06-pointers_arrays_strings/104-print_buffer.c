05198e9b3397d916e1ed72c2950608dbaa8e23d1
Hassan Cissey <hcissey0@gmail.com>
2023-03-30 16:42:59 +0000

    Buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,7 +12,7 @@ void print_buffer(char *b, int size)
 
 	if (size <= 0)
 	{
-		printf('\n');
+		printf("\n");
 		return;
 	}
 
@@ -37,6 +37,6 @@ void print_buffer(char *b, int size)
 			else
 				printf(".");
 		}
-		printf("\n');
+		printf("\n");
 	}
 }

587ac0d90282226e53aa55d40389aab7ab23dfec
Hassan Cissey <hcissey0@gmail.com>
2023-04-03 16:13:10 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * strpbrk - serches for any set of bytes
+ * @s: string to search through
+ * @accept: bytes to search for
+ * Return: character pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, found = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found)
+			return (s + i);
+	}
+	return (NULL);
+}

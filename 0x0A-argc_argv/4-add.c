032d4844f14fb1ddb81cf42a07e509ffc6965025
Hassan Cissey <hcissey0@gmail.com>
2023-04-07 16:55:55 -0700

    To Infinity And Beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -39,15 +39,14 @@ int toInt(char *s)
  */
 int main(int argc, char *argv[])
 {
+	int i, j, res = 0;
+
 	if (argc == 1)
 	{
 		_putchar('0');
 		_putchar('\n');
 		return (0);
 	}
-
-	int i, j, res = 0;
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; *(argv[i] + j) != '\0'; j++)

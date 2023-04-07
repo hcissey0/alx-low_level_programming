88a8f24450e52f3700ceb8c0311af83f888f93dc
Hassan Cissey <hcissey0@gmail.com>
2023-04-07 21:52:01 +0000

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of cli args
+ * @argv: cli args
+ *
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	_putchar('0' + argc - 1);
+	_putchar('\n');
+
+	return (0);
+}

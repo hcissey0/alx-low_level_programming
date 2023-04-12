74e49fec360dfa13db200f6e8f3b20ea9ac31967
Hassan Cissey <hcissey0@gmail.com>
2023-04-12 11:24:04 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,80 +2,33 @@
 #include <string.h>
 
 /**
+ * words - counts the number of words in the string
+ * @s: the string
+ * Return: string count
+ */
+int words(char *s)
+{
+	int i, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != ' ' && (i == 0 || s[i - 1] == ' '))
+			c++;
+	}
+	return (c);
+}
+
+/**
  * strtow - converts a string to array of words
  * @str: the string
  * Return: array of words
  */
 char **strtow(char *str)
 {
-	int i = 0, j = 0, k = 0, len = 0, rows = 0, cols = 0;
+	int i, j, k, w;
 	char **s;
 
-	if (str == NULL || strlen(str) < 1)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	len = strlen(str);
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] == ' ')
-		{
-			continue;
-		}
-		else
-		{
-			if (str[i - 1] ==  ' ')
-				rows++;
-		}
-	}
-	rows++;
-	s = malloc(sizeof(char *) * rows);
-	if (s == NULL)
-	{
-		free(s);
-		return (NULL);
-	}
-	cols = 0;
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] == ' ')
-		{
-			cols = 0;
-			continue;
-		}
-		else
-		{
-			cols++;
-			if (str[i + 1] == ' ' || str[i + 1] == '\0')
-			{
-				s[k++] = malloc(sizeof(char) * cols);
-				cols = 0;
-			}
-		}
-	}
-	j = 0;
-	i = 0;
-	k = 0;
-	cols = 0;
-	for (; i < rows - 1; i++)
-	{
-		for (; j < len; j++)
-		{
-			if (str[j] == ' ')
-			{
-				cols = 1;
-				break;
-			}
-			else
-			{
-				s[i][k++] = str[j];
-				cols = 0;
-			}
-		}
-		if (cols)
-		{
-			i--;
-			cols = 0;
-		}
-	}
-	s[rows - 1] = NULL;
-	return (s);
+	w = words(str);
 }

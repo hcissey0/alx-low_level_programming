ef8268880baa084283d3974dfe1d0fe65d6eed7d
Hassan Cissey <hcissey0@gmail.com>
2023-04-13 22:16:15 +0000

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,8 +29,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		s[i] = s2[j];
-	s[i] = '\0';
+	for (j = 0; j < n; j++)
+		s[i + j] = s2[j];
+	s[i + j] = '\0';
 	return (s);
 }

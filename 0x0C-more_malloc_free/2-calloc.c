86659ab1a2ab24d61e78ec50507170f10ff11e07
Hassan Cissey <hcissey0@gmail.com>
2023-04-13 22:05:32 +0000

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _calloc - imitates calloc
+ * @nmemb: nmemb
+ * @size: size of array
+ *
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+	return (s);
+}

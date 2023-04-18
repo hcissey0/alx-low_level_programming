348f4df7ba5f778424fd5d4d285739d18be52841
Hassan Cissey <hcissey0@gmail.com>
2023-04-18 04:51:00 +0000

    init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL || d->owner == NULL || !(age))
+	if (d->name == NULL || d->owner == NULL)
 		return;
 	(*d).name = name;
 	(*d).age = age;

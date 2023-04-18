d1ce06c6a4e1796537c27bb1d0e60f108a24095c
Hassan Cissey <hcissey0@gmail.com>
2023-04-18 05:27:02 +0000

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *dname, *downer;
+	int i, nl = 0, ol = 0;
 
 	d = malloc(sizeof(dog_t));
-	if (d == NULL || name == NULL || owner == NULL)
+	if (d == NULL)
+		return (NULL);
+	while (*(name + nl))
+		nl++;
+	while (*(owner + ol))
+		ol++;
+	dname = malloc(sizeof(char) * (nl + 1));
+	if (dname == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	downer = malloc(sizeof(char) * (ol + 1));
+	if (downer == NULL)
+	{
+		free(d);
+		free(dname);
+		return (NULL);
+	}
+	for (i = 0; i <= nl; i++)
+		dname[i] = name[i];
+	for (i = 0; i <= ol; i++)
+		downer[i] = owner[i];
+	d->name = dname;
+	d->age = age;
+	d->owner = downer;
 	return (d);
 }

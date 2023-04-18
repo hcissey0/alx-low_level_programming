0c1fc4d52b7f519b2da681857c15af1b0bcd1e32
Hassan Cissey <hcissey0@gmail.com>
2023-04-18 04:47:28 +0000

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(dog_t));
+	d = malloc(sizeof(dog_t *));
 	if (d == NULL || name == NULL || owner == NULL)
 	{
 		free(d);

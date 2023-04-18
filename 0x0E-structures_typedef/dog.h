92ec0d700c40c8b3a22447de5e2a9ed99ba13576
Hassan Cissey <hcissey0@gmail.com>
2023-04-18 02:49:37 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - this is a dog 
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif /* DOG_H */

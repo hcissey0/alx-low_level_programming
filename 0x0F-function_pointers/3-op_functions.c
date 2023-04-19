12de2e729f989be90460c04e62efa2cc76cb3da4
Hassan Cissey <hcissey0@gmail.com>
2023-04-19 12:14:11 +0000

    3-main.h added

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: returns the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: first number
+ * @b: second number
+ * Return: the subtraction of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: the product of the two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: first number
+ * @b: second number
+ * Return: the division of the two numbers
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a / b);
+}
+
+/** op_mod - find the modulo of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the modulo of the two integers
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a % b);
+}

599a6899a17c8374ebbcc2a8fcd8b7fdbfee2ccc
Hassan Cissey <hcissey0@gmail.com>
2023-04-19 13:00:23 +0000

    array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
+	if (array == NULL || action == NULL)
+		return;
 	while (i < (int) size)
 		(*action)(array[i++]);
 }

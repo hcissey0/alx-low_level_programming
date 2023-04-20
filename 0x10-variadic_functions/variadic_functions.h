4fcae53052dffc2c373773c432de83b577abb0cb
Hassan Cissey <hcissey0@gmail.com>
2023-04-20 08:31:28 +0000

    readme and variadic_functions.h added

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+#endif /* VARIADIC_FUNCTION_H */

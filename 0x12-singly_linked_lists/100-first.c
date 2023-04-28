7f7947c93da5dfba52d010572fcac8279da62f62
Hassan Cissey <hcissey0@gmail.com>
2023-04-28 00:42:18 +0000

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  * Return: void
  */
-void __attribute__((constructor)) first(void)
+void __attribute__((constructor))first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }

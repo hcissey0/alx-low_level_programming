6c40636574852d86fa5a03f4c27cb5914f56c650
Hassan Cissey <hcissey0@gmail.com>
2023-04-28 21:19:09 +0000

    print-listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of a linked list
+ * @h: head of the linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i = 0;
+	listint_t *ptr;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+
+	return (i);
+}

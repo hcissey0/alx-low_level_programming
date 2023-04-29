15f7de450e51c1c1055593597d31226a45734eca
Hassan Cissey <hcissey0@gmail.com>
2023-04-29 12:18:08 +0000

    free fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return;
 	ptr = *head;
-	while (ptr->next != NULL)
+	while (ptr != NULL)
 	{
 		temp = ptr;
 		ptr = ptr->next;

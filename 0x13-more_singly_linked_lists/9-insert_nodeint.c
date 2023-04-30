ccbcf0b0748cc84a84ae248420e018e475bde668
Hassan Cissey <hcissey0@gmail.com>
2023-04-30 22:49:06 +0000

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (i < idx - 1)
 	{
-		if (ptr == NULL || ptr->next == NULL)
+		if (ptr == NULL)
 			return (NULL);
 		ptr = ptr->next;
 		i++;

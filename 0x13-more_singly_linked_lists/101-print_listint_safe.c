6d6041a5629c19224a71ab37b77b0d78ce2507b7
Hassan Cissey <hcissey0@gmail.com>
2023-05-02 21:22:11 +0000

    print_listint_safe fixed, exit(98) added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,6 +11,8 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i = 0;
 	const listint_t *temp, *loop;
 
+	if (head == NULL)
+		exit(98);
 	while (head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);

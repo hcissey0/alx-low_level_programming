23cb8b77e1b62be2a98f293a49ab7b0139cedd45
Hassan Cissey <hcissey0@gmail.com>
2023-05-05 02:07:46 +0000

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks for the endianness
+ *
+ * Return: 1 if little endian, 0 if big endian
+ */
+int get_endianness(void)
+{
+	char endian[6] = "Endian";
+
+	if (&endian[0] > &endian[5])
+		return (1);
+	else if (&endian[0] < &endian[5])
+		return (0);
+}

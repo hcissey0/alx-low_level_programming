f126ba27d2de144ca6d800d710436ddaf3e4b3ed
Hassan Cissey <hcissey0@gmail.com>
2023-05-07 21:25:52 +0000

    print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,8 +11,6 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i = 0;
 	const listint_t *ptr = head, *temp = NULL;
 
-	if (head == NULL)
-		exit(98);
 	while (ptr != NULL)
 	{
 		printf("[%p] %d\n", (void *)ptr, ptr->n);

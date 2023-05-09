ae5716466075d2129d0ba9421281ffed3b35a930
Hassan Cissey <hcissey0@gmail.com>
2023-05-09 01:01:31 +0000

    read_textfile, hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,29 +9,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int r, w, fd;
+	ssize_t r, w;
+	int fd;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
 	r = read(fd, buffer, letters);
-	if (r < 0)
+	if (r == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 	buffer[r] = '\0';
-	w = write(STDOUT_FILENO, buffer, letters);
-	if (w < 0 || r != w)
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
 	{
 		free(buffer);
 		close(fd);

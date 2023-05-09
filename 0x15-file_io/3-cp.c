5427665040d67e61738a6e6f18bfb96a67dec468
Hassan Cissey <hcissey0@gmail.com>
2023-05-09 05:54:20 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,12 +9,14 @@
  * @m: the error message
  * @filename: the filename
  * @fd: the file des
+ * @e: exit status
  */
-void error(char *m, char *filename, int fd)
+void error(char *m, char *filename, int fd, int e)
 {
 	dprintf(STDERR_FILENO, m, filename);
 	if (fd != -1)
 		close(fd);
+	exit(e);
 }
 
 /**
@@ -35,33 +37,20 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
-
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		error("Error: Can't read from file %s\n", av[1], fd_from);
-		exit(98);
-	}
+		error("Error: Can't read from file %s\n", av[1], fd_from, 98);
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd-to == -1)
-	{
-		error("Error: Can't write to %s\n", av[2], fd_to);
-		exit(99);
-	}
+		error("Error: Can't write to %s\n", av[2], fd_to, 99);
 	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
-		{
-			error("Error: Can't write to %s\n", av[2], fd_to);
-			exit(99);
-		}
+			error("Error: Can't write to %s\n", av[2], fd_to, 99);
 	}
 	if (r == -1)
-	{
-		error("Error: Can't read from file %s\n", av[1], fd_from);
-		exit(98);
-	}
+		error("Error: Can't read from file %s\n", av[1], fd_from, 98);
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);

8e7d19b84424c17b2a1bade7f42b1f61e2663cf1
Hassan Cissey <hcissey0@gmail.com>
2023-05-08 18:30:07 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	r = strlen(text_content);
-	w = write(STDOUT_FILENO, text_content, r);
+	w = write(fd, text_content, r);
 	if (w == -1 || w != r)
 	{
 		close(fd);

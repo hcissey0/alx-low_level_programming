931f7a9410c6bdf514b69994b92614d5cfe6ba83
Hassan Cissey <hcissey0@gmail.com>
2023-05-10 20:34:45 -0400

    elf started

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <elf.h>
+
+#define ERR_EXIT_98 98
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

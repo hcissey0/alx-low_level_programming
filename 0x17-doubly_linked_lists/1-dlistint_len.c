d55b1ba7034f0472e80c851c7ca69dcac1b81e78
Hassan Cissey <hcissey0@gmail.com>
2023-06-15 08:39:16 +0000

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the length of a doubly linked list
+ * @h: head of the list
+ *
+ * Return: number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+	const dlistint_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		n++;
+		ptr = ptr->next;
+	}
+	return (n);
+}

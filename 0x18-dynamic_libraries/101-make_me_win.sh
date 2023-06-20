8fbdb85cdc9a12814983864c88be7825ff98b510
Hassan Cissey Tijani <hcissey0@gmail.com>
2023-06-19 20:27:08 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/hcissey0/alx-low_level_programming/0x018-dynamic_libraries/raw/master/libwin.so
+wget -P /tmp https://github.com/hcissey0/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
 export LD_PRELOAD=/tmp/libwin.so

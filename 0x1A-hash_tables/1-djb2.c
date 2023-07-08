bf9a3de83200c05cd82fae78bd1fb92b624e5408
Hassan Cissey <hcissey0@gmail.com>
2023-07-08 01:37:51 +0000

    djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/** hash_djb2 - The djb2 algorithm
+ * @str: The string to hash
+ *
+ * Return: the hash value of the string
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned int i;
+	unsigned long int h = 5381;
+
+	for (i = 0; str[i]; i++)
+		h = ((h << 5) + h) + str[i];
+	return (h);
+}

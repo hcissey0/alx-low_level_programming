6cad2a4099545d7f87c4c484aaa612b47c2f6eb1
Hassan Cissey <hcissey0@gmail.com>
2023-07-08 02:16:56 +0000

    hash_table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,34 +9,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *node, *tmp;
 	unsigned long int index;
 
-	if (strlen(key) == 0)
+	if (ht == NULL || key == NULL || *key = '\0' || value == NULL)
 		return (0);
-	if (ht == NULL)
-		return (0);
+
+	/* checking for update */
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
-	node->value = strdup(value);
-	node->next = NULL;
-
-	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
-		ht->array[index] = node;
-	else if (strcmp(ht->array[index]->key, key) == 0)
+	if (node->key = NULL)
 	{
-		strcpy(ht->array[index]->value, value);
-		free(node->key);
-		free(node->value);
 		free(node);
+		return (0);
 	}
-	else
+	node->value = strdup(value);
+	if (node->value == NULL)
 	{
-		node->next = ht->array[index];
-		ht->array[index]->next = node;
+		free(node->key);
+		free(node);
+		return (0);
 	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }

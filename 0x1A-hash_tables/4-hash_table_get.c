60d858031d653e1df1594bff163a49634a1d3a6c
Hassan Cissey <hcissey0@gmail.com>
2023-07-08 02:28:52 +0000

    hash_table_get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,4 +13,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (NULL);
-
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
